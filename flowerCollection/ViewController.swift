6564590d05d226b1613c4e39dc19cb7f4e90f895
cnet <cnet@cnet-no-MacBook-Pro.local>
2017-05-11 19:27:44 +0900

    a

--- flowerCollection/ViewController.swift
+++ flowerCollection/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var flowerImageView: UIImageView!
     
     var count:Int = 0
-    let flowerName: [String] = ["部屋１", "部屋２", "部屋３"]
+    let flowerName: [String] = ["部屋1", "部屋２", "部屋３"]
     let colors: [UIColor] = [UIColor.red, UIColor.blue, UIColor.yellow]
     let imageNames: [String] = ["image1", "image2", "image3"]
     
